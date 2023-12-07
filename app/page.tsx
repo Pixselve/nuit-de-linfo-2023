28665f2ada5a2e2aa758dfda7561e61bfd30aa2e
Thomega35 <thomega35@gmail.com>
2023-12-07 20:52:51 +0100

    Add amazing animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import WaldoButton from "@/components/waldo-button";
 import { motion } from 'framer-motion';
 
@@ -7,7 +8,16 @@ export default function Home() {
       <div className="h-full w-full bg-slate-200 p-10 prose">
         <div>
           {/* <motion.header initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}> */}
-          <h1>Ecological Information Website</h1>
+          <motion.div
+            whileHover={{ scale: 1.2, rotate: 90 }}
+            whileTap={{
+              scale: 0.8,
+              rotate: -90,
+              borderRadius: "100%"
+            }}
+          >
+            <h1>Ecological Information Website</h1>
+          </motion.div>
           {/* </motion.header> */}
           {/* <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.5 }}> */}
             <p>
