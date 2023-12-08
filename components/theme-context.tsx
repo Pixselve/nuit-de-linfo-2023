df9dddd1d9bd8b5d65f23fdc1b2d4b0a51ff80cc
Cody Adam <cody.ad4@gmail.com>
2023-12-08 03:11:32 +0100

    add theme context

---
+++ components/theme-context.tsx
@@ -0,0 +1,40 @@
+import { useState, useContext, createContext } from 'react';
+
+const themeContext = createContext<{
+  theme: "light" | "dark" | "emoji" | "colored";
+  patpat: boolean;
+  setTheme: (theme: "light" | "dark" | "emoji" | "colored") => void;
+  setPatpat: (patpat: boolean) => void;
+}>({
+  theme: "light",
+  patpat: false,
+  setTheme: () => { },
+  setPatpat: () => { },
+});
+
+export function useTheme() {
+  return useContext(themeContext);
+}
+
+export function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [theme, setTheme] = useState<"light" | "dark" | "emoji" | "colored">("light");
+  const [patpat, setPatpat] = useState<boolean>(false);
+
+
+  return (
+    <themeContext.Provider
+      value={{
+        theme,
+        patpat,
+        setTheme,
+        setPatpat,
+      }}
+    >
+      {children}
+    </themeContext.Provider>
+  );
+}
